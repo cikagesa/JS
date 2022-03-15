d5199da0c0ee51ed48786b5673ca5897c96be31d
Georgije Ignjatov <gignjatov@smart.edu.rs>
2022-03-15 17:11:50 +0100

    Operatori

---
+++ cas05/operatori.js
@@ -0,0 +1,16 @@
+function F(...niz){
+
+}
+F(4);
+F(5,6,7);
+
+
+function Suma(...brojevi){
+    var suma=0;
+    brojevi.forEach(function(value){
+        suma+=vaule;
+    }
+    )
+    return suma;
+
+}
